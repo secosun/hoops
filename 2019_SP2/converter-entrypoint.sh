6718158bac9476011dcaa1e009bcab67e526ec0f
Adam DuQuette <adam.duquette@techsoft3d.com>
2020-02-24 09:41:12 -0800

    Remove upsupported version, docker support will start at 2020 and move forward

--- 2019_SP2/converter-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/xvfb-run "--auto-servernum" "-s" "-screen 0 640x480x24" /opt/ts3d/bin/linux64/converter "$@"
--- 2019_SP2/csr-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/opt/ts3d/bin/linux64/ts3d_sc_server --csr true --sc-port 55555 --model-search-directories /opt/ts3d/models --workspace-dir /opt/ts3d/workspace "$@"
